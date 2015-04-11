48884f5e62ae0b34581af112770233e1a322e12f
Adam <adam.s@dine.to>
2015-04-11 11:43:30 -0400

    Add libjpeg-turbo, fix libjpegstream

--- jni/Android.mk
+++ jni/Android.mk
@@ -49,3 +49,39 @@ LOCAL_LDLIBS := -llog -ljnigraphics
 LOCAL_ARM_MODE := arm
 
 include $(BUILD_SHARED_LIBRARY)
+
+# Jpeg Streaming native
+include $(CLEAR_VARS)
+
+LOCAL_MODULE        := libjni_jpegstream
+
+LOCAL_NDK_STL_VARIANT := stlport_static
+
+LOCAL_C_INCLUDES := $(LOCAL_PATH) \
+                    $(LOCAL_PATH)/jpegstream \
+                    external/jpeg
+
+LOCAL_SHARED_LIBRARIES := libjpeg
+
+LOCAL_SDK_VERSION   := 9
+LOCAL_ARM_MODE := arm
+
+LOCAL_CFLAGS    += -ffast-math -O3 -funroll-loops
+LOCAL_CPPFLAGS += $(JNI_CFLAGS)
+LOCAL_LDLIBS := -llog
+
+LOCAL_CPP_EXTENSION := .cpp
+LOCAL_SRC_FILES     := \
+    jpegstream/inputstream_wrapper.cpp \
+    jpegstream/jpegstream.cpp \
+    jpegstream/jerr_hook.cpp \
+    jpegstream/jpeg_hook.cpp \
+    jpegstream/jpeg_writer.cpp \
+    jpegstream/jpeg_reader.cpp \
+    jpegstream/outputstream_wrapper.cpp \
+    jpegstream/stream_wrapper.cpp
+
+
+include $(BUILD_SHARED_LIBRARY)
+
+include $(LOCAL_PATH)/libjpeg-turbo/Android.mk
